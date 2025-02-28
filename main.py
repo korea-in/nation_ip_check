938226548c28cc4bfb29e772edc1663566883d46
korea-in <rudals2392@naver.com>
2025-02-28 20:56:38 +0900

    프로그램 개발 완료

--- main.py
+++ main.py
@@ -1,31 +1,67 @@
-# 판별하고자 하는 아이피 주소
-target_ip = "14.10.100.30"
+import math
+import time
 
-# KISA 사이트에서 다운받은 국가별 IP 대역대 파일 로드
-f = open("ipv4.csv", "r")
+start = time.time()
+
+nation_ip_arr = []
+target_ip_arr = []
+
+f1 = open("ipv4.csv", "r")
+f2 = open("data.txt", "r")
+f1.readline()
 
 def ipToInt(ip):
     octet1 = ip.split(".")[0]
     octet2 = ip.split(".")[1]
     octet3 = ip.split(".")[2]
     octet4 = ip.split(".")[3]
-    
     return int("%03d%03d%03d%03d" % (int(octet1), int(octet2), int(octet3), int(octet4)))
 
-# 첫줄은 항목명이 적혀있어 호출하여 제거한다.
-f.readline()
-
-# 1줄씩 읽으면서 아이피 대역대에 포함되는 국가 확인
-while True:
-    data = f.readline()
+# 20160515,KR,14.192.80.0,14.192.95.255,/20,20100920
+# { "nation_code": "KR", "from_ip": 14192080000, "to_ip": 14192095255 }
+while True: 
+    data = f1.readline().replace("\n", "")
+    if not data: break
+    tmp = {}
+    tmp["nation_code"] = data.split(",")[1]
+    tmp[    "from_ip"] = ipToInt(data.split(",")[2])
+    tmp[      "to_ip"] = ipToInt(data.split(",")[3])
+    nation_ip_arr.append(tmp)
+    
+while True: 
+    data = f2.readline().replace("\n", "")
     if not data: break 
+    target_ip_arr.append(ipToInt(data))
+
+# nation_ip_arr의 값인 object의 from_ip key로 정렬한다.
+# target_ip_arr는 정수형 데이터만 갖고 있음 그냥 정렬한다.
+nation_ip_arr.sort(key = lambda x :x["from_ip"])
+target_ip_arr.sort()
+
+etc_ip_cnt = 0 # 사설IP 개수
+nation_ip_cnt = {}
+idx = 0
+for i in range(len(target_ip_arr)):
+    target_ip = target_ip_arr[i]
+    for j in range(idx, len(nation_ip_arr)):
+        nation_ip = nation_ip_arr[j]
+        nation_code = nation_ip["nation_code"]
+        from_ip     = nation_ip["from_ip"]
+        to_ip       = nation_ip["to_ip"]
+
+        if((target_ip >= from_ip) and (target_ip <= to_ip)):
+            if nation_ip_cnt.get(nation_code) == None:
+                nation_ip_cnt[nation_code] = 1
+            else:
+                nation_ip_cnt[nation_code] = nation_ip_cnt[nation_code] + 1
+            break
+        if(nation_ip == nation_ip_arr[len(nation_ip_arr)-1]):
+            etc_ip_cnt = etc_ip_cnt + 1
+    idx = j - 1
+
+sorted_nation_ip_cnt = sorted(nation_ip_cnt, key= lambda x: nation_ip_cnt[x], reverse=True)
+for i in range(5):
+    print("%d번째 유입량이 많은 국가코드: %s, 유입량: %d건" %(i+1, sorted_nation_ip_cnt[i], nation_ip_cnt[sorted_nation_ip_cnt[i]]))
     
-    # csv 파일은 콤마(,)로 데이터를 구분하기 때문에 split 함수로 필요한 부분만 추출한다.
-    nation_code = data.split(",")[1]
-    from_ip     = data.split(",")[2]
-    to_ip       = data.split(",")[3]
-    
-    # target_ip가 from_ip ~ to_ip 사이에 들어가 있는지 검증한다.
-    if((ipToInt(target_ip) >= ipToInt(from_ip)) and (ipToInt(target_ip) <= ipToInt(to_ip))):
-        print("IP(%s)가 속한 대역대를 확인하였습니다. [%s]국가, [%s] ~ [%s] 대역대" % (target_ip, nation_code, from_ip, to_ip))
-        break
+end = time.time()
+print("총 실행시간은 %.2f초" % (end - start))
